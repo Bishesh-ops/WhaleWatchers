fa9ce0893a9770b55e200b623cf6325b642b1cb6
Bisheshops <bisheshshr@gmail.com>
2026-05-07 13:49:05 +0545

    Add CORS policy to allow all origins, methods, and headers

--- ClientGateway/Program.cs
+++ ClientGateway/Program.cs
@@ -1,14 +1,26 @@
 using ClientGateway.Data;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("WhaleDb")));
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        corsBuilder => corsBuilder.AllowAnyOrigin()
+                                  .AllowAnyMethod()
+                                  .AllowAnyHeader());
+});
+
+
 builder.Services.AddControllers();
+
 var app = builder.Build();
 
+app.UseCors("AllowAll");
+
+// 5. Map the endpoints and run the server
 app.MapControllers();
 app.Run("http://localhost:5000");
